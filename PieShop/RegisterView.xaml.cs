a9d0556fe341ea973bc1aadae47affc5f4a1b840
Taylor Cox <taylorashtoncox@gmail.com>
2019-09-06 14:59:20 -0400

    Adds validation and more functionality to registration view

--- PieShop/RegisterView.xaml.cs
+++ PieShop/RegisterView.xaml.cs
@@ -10,6 +10,31 @@ namespace PieShop
         public RegisterView()
         {
             InitializeComponent();
+            RegisterButton.IsEnabled = false;
+        }
+
+
+        private async void RegisterButton_OnClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Success", "Your account has been created", "Done");
+        }
+
+        private void UsernameEntry_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            RegisterButton.IsEnabled = ValidateRegistration();
+        }
+
+        private void PasswordEntry_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+
+            RegisterButton.IsEnabled = ValidateRegistration();
+        }
+
+        private bool ValidateRegistration()
+        {
+            if (UsernameEntry.Text?.Length > 0 && PasswordEntry.Text?.Length > 0)
+                return true;
+                return false;
         }
     }
 }
